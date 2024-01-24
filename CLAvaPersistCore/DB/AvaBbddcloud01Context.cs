71b96e97d26b30fdc80c92159d0425f1d9f61cf2
Carlos Gimeno Ortiz <105145931+cgimeno00@users.noreply.github.com>
2024-01-24 11:52:49 +0100

    Connection string deleted

--- CLAvaPersistCore/DB/AvaBbddcloud01Context.cs
+++ CLAvaPersistCore/DB/AvaBbddcloud01Context.cs
@@ -25,7 +25,7 @@ public partial class AvaBbddcloud01Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=avasqlserver001.database.windows.net; Database=AvaBBDDCloud01; User Id=AvaPersistAdmin001; Password=avapersist@001;");
+        => optionsBuilder.UseSqlServer("PONER CONNECTION STRING");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
